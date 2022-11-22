df0fc3f9e7d46f35d06cfe1fa9b4967875b6ce19
Ian Naylan <42600121+i1d9@users.noreply.github.com>
2022-11-22 15:44:00 +0300

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Ian Nalyanya on 22/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
